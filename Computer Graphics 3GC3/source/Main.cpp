a91e42c13309f4619ef0a979968d408d3e028e0f
icecapped <1027Michael@gmail.com>
2024-10-16 01:24:48 -0400

    2024 edition

--- Computer Graphics 3GC3/source/Main.cpp
+++ Computer Graphics 3GC3/source/Main.cpp
@@ -20,7 +20,7 @@ float get_moon_rotate_angle_around_itself(float day);
 
 // settings
 const unsigned int SCR_WIDTH = 1024;
-const unsigned int SCR_HEIGHT = 768;
+const unsigned int SCR_HEIGHT = 576;
 
 const char* vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
@@ -42,21 +42,42 @@ const char* fragmentShaderSource = "#version 330 core\n"
 "   FragColor = vec4(Colour.x, Colour.y, Colour.z, 1.0f);\n"
 "}\n\0";
 
+/*
+    Build, compile, and link the shader
+ */
+bool setupShader(GLuint shaderProgram, GLenum shaderType, const char** shaderSource) {
+    GLuint shader = glCreateShader(shaderType);
+    glShaderSource(shader, 1, shaderSource, NULL);
+    glCompileShader(shader);
+
+    // check for shader compile errors
+    int success;
+    char infoLog[512];
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(shader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::COMPILATION_FAILED\n" << infoLog << std::endl;
+        return false;
+    }
+
+    // link shader
+    glAttachShader(shaderProgram, shader);
+    return true;
+}
+
 int main()
 {
-    // glfw: initialize and configure
-    // ------------------------------
+    // glfw init
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-#ifdef __APPLE__
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-#endif
+    #ifdef __APPLE__
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    #endif
 
-    // glfw window creation
-    // --------------------
+    // glfw window 
     GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
     if (window == NULL)
     {
@@ -67,107 +88,98 @@ int main()
     glfwMakeContextCurrent(window);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-    // glad: load all OpenGL function pointers
-    // ---------------------------------------
+    // glad init
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
 
+
     // configure global opengl state
     // -----------------------------
-    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_DEPTH_TEST);    
+    glDepthFunc(GL_LESS);
 
-    // build and compile our shader program
-    // ------------------------------------
+
+    // SHADER BUILD, COMPILE, LINK
+    unsigned int shaderProgram = glCreateProgram();
+
     // vertex shader
-    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-    glCompileShader(vertexShader);
-    // check for shader compile errors
-    int success;
-    char infoLog[512];
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    if (!setupShader(shaderProgram, GL_VERTEX_SHADER, &vertexShaderSource)) {
+        std::cout << "Stopping because of vertex shader compilation error";
     }
     // fragment shader
-    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-    glCompileShader(fragmentShader);
-    // check for shader compile errors
-    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    if (!setupShader(shaderProgram, GL_FRAGMENT_SHADER, &fragmentShaderSource)) {
+        std::cout << "Stopping because of fragment shader compilation error";
     }
+
     // link shaders
-    unsigned int shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
     glLinkProgram(shaderProgram);
     // check for linking errors
+    int success;
+    char infoLog[512];
     glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
     if (!success) {
         glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
         std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
     }
 
-    // set up vertex data (and buffer(s)) and configure vertex attributes
+
+    // set up vertex data (and buffer(s)) and configure vertex attributes for cube
     // ------------------------------------------------------------------
+    // order: -z, +z, -x, +x, -y, +y
+    // back front left right bottom top
     float vertices[] = {
+        -1.0f, -1.0f, -1.0f,  0.0f, 1.0f, 0.0f,
+         1.0f, -1.0f, -1.0f,  0.0f, 1.0f, 0.0f,
+         1.0f,  1.0f, -1.0f,  0.0f, 1.0f, 0.0f,
+         1.0f,  1.0f, -1.0f,  0.0f, 1.0f, 0.0f,
+        -1.0f,  1.0f, -1.0f,  0.0f, 1.0f, 0.0f,
+        -1.0f, -1.0f, -1.0f,  0.0f, 1.0f, 0.0f,
+
+        -1.0f, -1.0f,  1.0f,  1.0f, 0.0f, 0.0f,
+         1.0f, -1.0f,  1.0f,  1.0f, 0.0f, 0.0f,
+         1.0f,  1.0f,  1.0f,  1.0f, 0.0f, 0.0f,
+         1.0f,  1.0f,  1.0f,  1.0f, 0.0f, 0.0f,
+        -1.0f,  1.0f,  1.0f,  1.0f, 0.0f, 0.0f,
+        -1.0f, -1.0f,  1.0f,  1.0f, 0.0f, 0.0f,
+
+        -1.0f,  1.0f,  1.0f,  0.0f, 0.0f, 1.0f,
+        -1.0f,  1.0f, -1.0f,  0.0f, 0.0f, 1.0f,
+        -1.0f, -1.0f, -1.0f,  0.0f, 0.0f, 1.0f,
+        -1.0f, -1.0f, -1.0f,  0.0f, 0.0f, 1.0f,
+        -1.0f, -1.0f,  1.0f,  0.0f, 0.0f, 1.0f,
+        -1.0f,  1.0f,  1.0f,  0.0f, 0.0f, 1.0f,
+
+         1.0f,  1.0f,  1.0f,  0.0f, 1.0f, 1.0f,
+         1.0f,  1.0f, -1.0f,  0.0f, 1.0f, 1.0f,
+         1.0f, -1.0f, -1.0f,  0.0f, 1.0f, 1.0f,
+         1.0f, -1.0f, -1.0f,  0.0f, 1.0f, 1.0f,
+         1.0f, -1.0f,  1.0f,  0.0f, 1.0f, 1.0f,
+         1.0f,  1.0f,  1.0f,  0.0f, 1.0f, 1.0f,
+
         -1.0f, -1.0f, -1.0f,  1.0f, 1.0f, 0.0f,
          1.0f, -1.0f, -1.0f,  1.0f, 1.0f, 0.0f,
-         1.0f,  1.0f, -1.0f,  1.0f, 1.0f, 0.0f,
-         1.0f,  1.0f, -1.0f,  1.0f, 1.0f, 0.0f,
-        -1.0f,  1.0f, -1.0f,  1.0f, 1.0f, 0.0f,
+         1.0f, -1.0f,  1.0f,  1.0f, 1.0f, 0.0f,
+         1.0f, -1.0f,  1.0f,  1.0f, 1.0f, 0.0f,
+        -1.0f, -1.0f,  1.0f,  1.0f, 1.0f, 0.0f,
         -1.0f, -1.0f, -1.0f,  1.0f, 1.0f, 0.0f,
 
-        -1.0f, -1.0f,  1.0f,  1.0f, 0.0f, 1.0f,
-         1.0f, -1.0f,  1.0f,  1.0f, 0.0f, 1.0f,
+        -1.0f,  1.0f, -1.0f,  1.0f, 0.0f, 1.0f,
+         1.0f,  1.0f, -1.0f,  1.0f, 0.0f, 1.0f,
          1.0f,  1.0f,  1.0f,  1.0f, 0.0f, 1.0f,
          1.0f,  1.0f,  1.0f,  1.0f, 0.0f, 1.0f,
         -1.0f,  1.0f,  1.0f,  1.0f, 0.0f, 1.0f,
-        -1.0f, -1.0f,  1.0f,  1.0f, 0.0f, 1.0f,
-
-        -1.0f,  1.0f,  1.0f,  1.0f, 0.0f, 0.0f,
-        -1.0f,  1.0f, -1.0f,  1.0f, 0.0f, 0.0f,
-        -1.0f, -1.0f, -1.0f,  1.0f, 0.0f, 0.0f,
-        -1.0f, -1.0f, -1.0f,  1.0f, 0.0f, 0.0f,
-        -1.0f, -1.0f,  1.0f,  1.0f, 0.0f, 0.0f,
-        -1.0f,  1.0f,  1.0f,  1.0f, 0.0f, 0.0f,
-
-         1.0f,  1.0f,  1.0f,  0.0f, 1.0f, 0.0f,
-         1.0f,  1.0f, -1.0f,  0.0f, 1.0f, 0.0f,
-         1.0f, -1.0f, -1.0f,  0.0f, 1.0f, 0.0f,
-         1.0f, -1.0f, -1.0f,  0.0f, 1.0f, 0.0f,
-         1.0f, -1.0f,  1.0f,  0.0f, 1.0f, 0.0f,
-         1.0f,  1.0f,  1.0f,  0.0f, 1.0f, 0.0f,
-
-        -1.0f, -1.0f, -1.0f,  0.0f, 1.0f, 1.0f,
-         1.0f, -1.0f, -1.0f,  0.0f, 1.0f, 1.0f,
-         1.0f, -1.0f,  1.0f,  0.0f, 1.0f, 1.0f,
-         1.0f, -1.0f,  1.0f,  0.0f, 1.0f, 1.0f,
-        -1.0f, -1.0f,  1.0f,  0.0f, 1.0f, 1.0f,
-        -1.0f, -1.0f, -1.0f,  0.0f, 1.0f, 1.0f,
-
-        -1.0f,  1.0f, -1.0f,  0.0f, 0.0f, 1.0f,
-         1.0f,  1.0f, -1.0f,  0.0f, 0.0f, 1.0f,
-         1.0f,  1.0f,  1.0f,  0.0f, 0.0f, 1.0f,
-         1.0f,  1.0f,  1.0f,  0.0f, 0.0f, 1.0f,
-        -1.0f,  1.0f,  1.0f,  0.0f, 0.0f, 1.0f,
-        -1.0f,  1.0f, -1.0f,  0.0f, 0.0f, 1.0f,
+        -1.0f,  1.0f, -1.0f,  1.0f, 0.0f, 1.0f,
     };
+
     unsigned int VBO, VAO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
 
+    // setup cube VAO
     glBindVertexArray(VAO);
-
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
@@ -177,76 +189,109 @@ int main()
     // texture coord attribute
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
+    
+    //unbind to finish
+    glBindVertexArray(0);
 
     // render loop
     // -----------
-    float day = 365, inc = 1.0f/24;
+    float day = 365.0f, inc = 1.0f/24/60;
     while (!glfwWindowShouldClose(window))
     {
-        // input
-        // -----
+        // INPUT
         processInput(window);
+
+
+        // "PHYSICS" LOOP
         day += inc;
-        // render
-        // ------
+
+        
+        // SETUP M-V-P MATRICES
+        glm::mat4 model = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
+        glm::mat4 view = glm::mat4(1.0f);
+        glm::mat4 projection = glm::mat4(1.0f);
+
+
+        // MODEL MATRIX
+        // sun
+        glm::mat4 suntrans = glm::mat4(1.0f);
+        suntrans = glm::rotate(suntrans, glm::radians(get_sun_rotate_angle_around_itself(day)), glm::vec3(0.0f, 1.0f, 0.0f));       // rotation
+        suntrans = glm::scale(suntrans, glm::vec3(10.0f, 10.0f, 10.0f));
+
+        // earth position
+        glm::mat4 earthpos = glm::mat4(1.0f); // post-orbit location for moon
+        earthpos = glm::rotate(earthpos, glm::radians(get_earth_rotate_angle_around_sun(day)), glm::vec3(0.0f, 1.0f, 0.0f));        // orbit rotation
+        earthpos = glm::translate(earthpos, glm::vec3(40.0f, 0.0f, 0.0f));                                                          // orbit offset
+        earthpos = glm::rotate(earthpos, -glm::radians(get_earth_rotate_angle_around_sun(day)), glm::vec3(0.0f, 1.0f, 0.0f));       // rotation compensation for orbit rotation
+
+        // earth rotation
+        glm::mat4 earthtrans = glm::mat4(1.0f);
+        earthtrans = glm::rotate(earthtrans, glm::radians(23.4f), glm::vec3(0.0f, 0.0f, -1.0f));                                    // axis offset
+        earthtrans = glm::rotate(earthtrans, glm::radians(get_earth_rotate_angle_around_itself(day)), glm::vec3(0.0f, 1.0f, 0.0f)); // rotation
+        earthtrans = glm::scale(earthtrans, glm::vec3(4.0f, 4.0f, 4.0f));
+        earthtrans = earthpos * earthtrans;
+
+        // moon
+        glm::mat4 moontrans = glm::mat4(1.0f);
+        moontrans = glm::rotate(moontrans, glm::radians(get_moon_rotate_angle_around_earth(day)), glm::vec3(0.0f, 1.0f, 0.0f));     // orbit rotation
+        moontrans = glm::translate(moontrans, glm::vec3(12.0f, 0.0f, 0.0f));                                                        // orbit offset
+        moontrans = glm::rotate(moontrans, -glm::radians(get_moon_rotate_angle_around_earth(day)), glm::vec3(0.0f, 1.0f, 0.0f));    // orbit compensation
+        moontrans = glm::rotate(moontrans, glm::radians(get_moon_rotate_angle_around_itself(day)), glm::vec3(0.0f, 1.0f, 0.0f));    // rotation
+        moontrans = glm::scale(moontrans, glm::vec3(2.0f, 2.0f, 2.0f));
+        moontrans = earthpos * moontrans;   // relative to earth position
+
+
+        // RENDER SETUP
         glClearColor(0.3f, 0.4f, 0.5f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // also clear the depth buffer now!
 
         glUseProgram(shaderProgram);
 
+
+        // VIEW MATRIX
         // camera position
-        glm::vec3 cameraPos = glm::vec3(80.0f, 70.0f, 60.0f);
+        glm::vec3 cameraPos = glm::vec3(30.0f, 20.0f, 60.0f);
+        // camera targets
+        glm::vec3 sunpos_vec3 = glm::vec3(0.0f);
+        glm::vec3 earthpos_vec3 = glm::vec3(earthpos * glm::vec4(1.0f));
+        glm::vec3 moonpos_vec3 = glm::vec3(moontrans * glm::vec4(1.0f));
 
-        // create transformations
-        glm::mat4 model = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
-        glm::mat4 view = glm::mat4(1.0f);
-        glm::mat4 projection = glm::mat4(1.0f);
-        view = glm::lookAt(cameraPos, glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        view = glm::lookAt(cameraPos, sunpos_vec3, glm::vec3(0.0f, 1.0f, 0.0f));
 
-        // looking at earth
-        glm::mat4 earthtrans = glm::mat4(1.0f);
-        glm::mat4 earthloc = glm::mat4(1.0f);
-        earthtrans = glm::rotate(earthtrans, glm::radians(get_earth_rotate_angle_around_sun(day)), glm::vec3(0.0f, 0.0f, -1.0f));
-        earthtrans = glm::translate(earthtrans, glm::vec3(24.0f, 0.0f, 0.0f));
-        earthloc = earthtrans;
 
-        //glm::vec4 earthlocvec = glm::vec4(1.0f);
-        //earthlocvec = earthloc * earthlocvec;
-        //view = glm::lookAt(cameraPos, glm::vec3(earthlocvec), glm::vec3(0.0f, 1.0f, 0.0f));
-
-        projection = glm::perspective(glm::radians(30.0f), (float)4 / (float)3, 0.1f, 1000.0f);
+        // PROJECTION MATRIX
+        // perspective
+        projection = glm::perspective(glm::radians(90.0f), (float)16 / (float)9, 0.1f, 1000.0f);
+        // ortho
+        // projection = glm::ortho(0.0f, 16.0f/9.0f * 70.0f, -35.0f, 35.0f, 0.1f, 1000.0f); 
+        
+        
+        // SET SHADER MATRICES
         // retrieve the matrix uniform locations
         unsigned int modelLoc = glGetUniformLocation(shaderProgram, "model");
         unsigned int viewLoc = glGetUniformLocation(shaderProgram, "view");
         unsigned int projLoc = glGetUniformLocation(shaderProgram, "projection");
         // pass them to the shaders (3 different ways)
-        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+        //glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
         glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &view[0][0]);
         glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
 
+
+        // RENDER OBJECTS
         glBindVertexArray(VAO);
 
         // render sun
-        glm::mat4 suntrans = glm::mat4(1.0f);
-        suntrans = glm::scale(suntrans, glm::vec3(6.0f, 6.0f, 6.0f));
-        suntrans = glm::rotate(suntrans, glm::radians(get_sun_rotate_angle_around_itself(day)), glm::vec3(0.0f, 0.0f, -1.0f));
         glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(suntrans));
         glDrawArrays(GL_TRIANGLES, 0, 36);
 
         // render earth
-        earthtrans = glm::rotate(earthtrans, glm::radians(23.4f+get_earth_rotate_angle_around_itself(day) - get_earth_rotate_angle_around_sun(day)), glm::vec3(0.0f, 0.0f, -1.0f));
-        earthtrans = glm::scale(earthtrans, glm::vec3(3.0f, 3.0f, 3.0f));
         glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(earthtrans));
         glDrawArrays(GL_TRIANGLES, 0, 36);
 
         // render moon
-        glm::mat4 moontrans = earthloc;
-        moontrans = glm::rotate(moontrans, glm::radians(get_moon_rotate_angle_around_earth(day)), glm::vec3(0.0f, 0.0f, -1.0f));
-        moontrans = glm::translate(moontrans, glm::vec3(12.0f, 0.0f, 0.0f));
-        //moontrans = glm::rotate(moontrans, glm::radians(get_moon_rotate_angle_around_itself(day)), glm::vec3(0.0f, 0.0f, -1.0f));
-        moontrans = glm::scale(moontrans, glm::vec3(1.5f, 1.5f, 1.5f));
         glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(moontrans));
         glDrawArrays(GL_TRIANGLES, 0, 36);
+        
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
@@ -275,15 +320,15 @@ float get_earth_rotate_angle_around_sun(float day) {
     return 360.0f / 365.0f * day;
 }
 float get_earth_rotate_angle_around_itself(float day) {
-    return 360.0f / 24.0f * day;
+    return 360.0f * day;
 }
 
 // moon
 float get_moon_rotate_angle_around_earth(float day) {
-    return 360.0f / 27.0f * day;
+    return 360.0f / 28.0f * day;
 }
 float get_moon_rotate_angle_around_itself(float day) {
-    return 360.0f / 27.0f * day;
+    return 360.0f / 28.0f * day;
 }
 
 static unsigned int ss_id = 0;
